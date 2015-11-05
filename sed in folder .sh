8b96d49005788bf35dfc100c9cb029ce66ea7551
vatoer <vatoer.ckplus@gmail.com>
2015-11-05 14:28:27 +0300

    Update sed in folder .sh
    ok

--- sed in folder .sh
+++ sed in folder .sh
@@ -1,3 +1,5 @@
 #bin/bash
 # cari '"js/' ganti dengan '"/js/'
 find ./ -type f -exec sed -i -e 's/\"\js\//\"\/js\//g' {} \;
+# cari 'kemanan' ganti dengan 'keamanan'
+find ./ -type f -exec sed -i -e 's/kemanan/keamanan/g' {} \;
